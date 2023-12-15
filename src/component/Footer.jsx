675ba954308cd23c4183c083dd179b83675bc39e
elm-Saad <128216930+elm-Saad@users.noreply.github.com>
2023-12-15 20:26:48 +0100

    update

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,32 +1,46 @@
-
-
-/** */
+import { IoStorefront } from "react-icons/io5"
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { Link } from "react-router-dom"
 
 
 const Footer = ()=>{
     return  <section className="bg-neutral mt-16">
-            <footer className="footer p-10 text-neutral-content align-elm py-4">
-                <nav>
-                    <header className="footer-title">Services</header> 
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
-                </nav> 
+            <div className="align-elm py-4">
+            <footer className="footer p-10 text-neutral-content">
                 <nav>
                     <header className="footer-title">Company</header> 
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <Link
+                     className="link link-hover"
+                     to={'/about'}
+                    >
+                        About us
+                    </Link>
                 </nav> 
                 <nav>
-                    <header className="footer-title">Legal</header> 
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+                    <header className="footer-title">STORE INFO</header> 
+                    <a href="#" className="link link-hover">Store Demo Store USA</a>
+                    <a href="#" className="link link-hover">+123456789</a>
+                    <a href="#" className="link link-hover">Support@test.Com</a>
+                </nav>
+            </footer> 
+            <footer className="footer px-10 py-4 border-t text-neutral-content border-neutral-300">
+                <aside className="items-center grid-flow-col">
+                    <IoStorefront className="text-5xl pl-4" />
+                    <p>Astro Store<br/>Providing reliable tech since 2023</p>
+                </aside> 
+                <nav className="md:place-self-center md:justify-self-end">
+                    <div className="grid grid-flow-col gap-4">
+                        <a href="#" target="_blank">
+                            <FaTwitter className="text-xl"/>
+                        </a>
+                        <a href="#" target="_blank">
+                            <FaLinkedinIn className="text-xl" />
+                        </a>
+                    </div>
                 </nav>
             </footer>
+            </div>
         </section>
    
 }
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -19,7 +19,6 @@ export const loader = (queryClient) => async () => {
 }
 
 
-
 const Landing = ()=>{
     return <>
         <Hero />
